3279ea1fa62149832d7ac2e9f09452ef4d00ea65
ridhanadhirafebyani <ridha_febyani_24rpl@student.smktelkom-mlg.sch.id>
2017-01-28 15:20:28 -0800

    test array dua

---
+++ src/testarraydua/Testarraydua.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testarraydua;
+
+/**
+ *
+ * @author Ridha Nadhira Febyan
+ */
+public class Testarraydua {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+        // TODO code application logic here
+        int nis[][]={{210651},{210651},{210652}};
+        String nama[][]={{"Harun Ulum"},{"Nur Azmi"},{"Devita Fahma"}};
+        public void tampil(String n){
+            System.out.println("Identitas Siswa Angkatan 24");
+        }
+        public void namanim(int n){
+            for(int i=0;i<1;i++){
+                for (int j=0; j<1;j++){
+                    System.out.println(nama[1][j]+":"+nis[1][j]);
+                }
+            }
+        }
+        public static void main(String []args){
+            Testarraydua siswa =new Testarraydua();
+            siswa.tampil(null);
+            siswa.namanim(0);
+        }
+    }
+ 
+
